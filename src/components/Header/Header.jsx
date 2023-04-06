b6dc9358de2274799ffc103260fde66a0a7c1439
anas0 <ashraful0a@gmail.com>
2023-04-06 19:45:41 +0600

    about added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BoltIcon } from '@heroicons/react/24/solid'
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='bg-gray-100 flex items-center justify-between'>
+            <Link to='/' className='inline-flex items-center'>
+                <BoltIcon className='h-7 w-7 text-blue-500' />
+                <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>nextPage</span>
+            </Link>
+            <ul className='items-center hidden space-x-8 lg:flex'>
+                <li>
+                    <NavLink
+                    to='/'
+                    className={({ isActive }) => (isActive ? 'active' : 'default')}
+                    >
+                    Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                    to='/books'
+                    className={({ isActive }) => (isActive ? 'active' : 'default')}
+                    >
+                    Books
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                    to='/about'
+                    className={({ isActive }) => (isActive ? 'active' : 'default')}
+                    >
+                    About us
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Books from './components/Books/Books'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
-  }
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'books',
+        element: <Books />
+      },
+      {
+        path: '/about',
+        element: <About></About>
+      }
+    ]
+  }  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
